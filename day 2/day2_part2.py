7fe2c2c48722ba2fcdde6ebab488ac5d3ef32a91
Nguyen Kim Ngan <kn30805002@gmail.com>
2026-01-07 17:34:18 +0100

    Implement range processing and summation logic
    Read ranges from 'id.txt' and sum numbers with even length that have equal halves.

---
+++ day 2/day2_part2.py
@@ -0,0 +1,17 @@
+with open ("id.txt", "r") as file:
+    som = 0
+    for ranges in file:
+        ranges = ranges.split(",")
+        for parts in ranges:
+            numbers = parts.split("-")
+            start = int(numbers[0])
+            end = int(numbers[1])
+            for i in range(start, end + 1):
+                s = str(i)
+                if len(s) % 2 == 0:
+                    midpoint = len(s) // 2
+                    first_half = s[:midpoint]
+                    second_half = s[midpoint:]
+                    if first_half == second_half:
+                        som += i
+    print(som)
